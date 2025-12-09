419ee0188cede57d0667c485485fdfea6a6098a4
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2025-12-08 20:49:48 -0500

    Progress

--- issues.ml
+++ issues.ml
@@ -9,17 +9,31 @@ Usage:
 Issues marked as CREATED upon confirmation.
 
 "
+
+let prepare_message comment issue_number line_number =
+  ignore(Printf.sprintf "
+
+  Title: ISSUE # %d
+
+  LineNo : %d
+  Message: %s
+
+  " issue_number line_number comment);
+  ()
+
 let push_issues_to_git_tracker issues = 
   Printf.printf "%s" issue_prompt_string;
   let rec print_issue issues = 
     match issues with
     | (comment, lineno, true) :: tail ->
-        Printf.printf "%4d. %s\n" lineno comment;
+        Printf.printf "\n%4d. %s\n" lineno comment;
         Printf.printf "\nConfirm [y/yes to create, else skip]: ";
         let line = read_line() in
-        Printf.printf "Input: %s\n" line;
-        print_issue tail
-    | (comment, _, false) :: tail -> 
+          if String.equal line "y" || String.equal line "yes" then begin
+            Printf.printf "Creating Issue\n";
+            print_issue tail
+          end
+    | (_, _, false) :: tail -> 
         print_issue tail;
     | [] -> ()
   in
--- samples/sample.c
+++ samples/sample.c
@@ -2,6 +2,7 @@ int main(){
   // Writing some C code. YAY !! 
   //
   // TODO: Make it work. @ISSUE(#123)
+  // TODO: Make it work. @ISSUE(#255)
   return 0;
 }
 
